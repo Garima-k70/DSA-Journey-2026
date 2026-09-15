b708c760cdcc3db0a097b0167b3cb109327f9d39
Garima-k70 <garimakush70@gmail.com>
2026-09-15 23:32:24 +0530

    Implement maxPalindromes method for palindrome counting

---
+++ Dynamic-Programming/Maximum-Number-of-Non-Overlapping-Palindromes/Solution.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public int maxPalindromes(String s, int k) {
+
+        int n = s.length();
+
+        // palindrome[i][j] = true if s[i...j] is a palindrome
+        boolean[][] palindrome = new boolean[n][n];
+
+        // Build palindrome table
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = i; j < n; j++) {
+
+                if (s.charAt(i) == s.charAt(j) &&
+                    (j - i <= 2 || palindrome[i + 1][j - 1])) {
+
+                    palindrome[i][j] = true;
+                }
+            }
+        }
+
+        // dp[i] = maximum number of valid palindromes
+        // using the first i characters
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            // Don't use a palindrome ending at i-1
+            dp[i] = dp[i - 1];
+
+            // Try every possible starting position
+            for (int start = 0; start <= i - k; start++) {
+
+                if (palindrome[start][i - 1]) {
+
+                    dp[i] = Math.max(
+                        dp[i],
+                        dp[start] + 1
+                    );
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
